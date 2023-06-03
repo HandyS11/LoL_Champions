5e083134e9dce70af9b1e83bfebeae455b9937e5
HandyS11 <62420910+HandyS11@users.noreply.github.com>
2023-06-03 18:44:59 +0200

    Improve views and styles

---
+++ Sources/LoL_Champions/ViewModels/AddOrEditChampionNavigationVM.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace LoL_Champions.ViewModels
+{
+    public class AddOrEditChampionNavigationVM
+    {
+        private readonly INavigation _navigation;
+
+        public ICommand NavigateBackCommand { get; private set; }
+
+        public AddOrEditChampionNavigationVM(INavigation navigation)
+        {
+            _navigation = navigation;
+
+            NavigateBackCommand = new Command(async () => await NavigateBack());
+        }
+
+        private async Task NavigateBack()
+        {
+            await _navigation.PopAsync();
+        }
+    }
+}
--- Sources/LoL_Champions/Views/Pages/AddOrEditChampionPage.xaml.cs
+++ Sources/LoL_Champions/Views/Pages/AddOrEditChampionPage.xaml.cs
@@ -1,9 +1,15 @@
+using LoL_Champions.ViewModels;
+
 namespace LoL_Champions.Views.Pages;
 
 public partial class AddOrEditChampionPage : ContentPage
 {
+	public AddOrEditChampionNavigationVM _navigation { get; private set; }
+
 	public AddOrEditChampionPage()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
 	}
 }
