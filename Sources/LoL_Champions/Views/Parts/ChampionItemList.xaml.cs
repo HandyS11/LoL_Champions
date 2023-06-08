9abf80aa6a1ca33f063dbec77705f8b6296a97c6
Valentin Clergue <valentin.clergue@etu.uca.fr>
2023-06-08 09:15:08 +0200

    fix delete command (still bugged)

--- Sources/LoL_Champions/Views/Parts/ChampionItemList.xaml.cs
+++ Sources/LoL_Champions/Views/Parts/ChampionItemList.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows.Input;
+
 namespace LoL_Champions.Views.Parts;
 
 public partial class ChampionItemList : ViewCell
@@ -6,4 +8,22 @@ public partial class ChampionItemList : ViewCell
     {
         InitializeComponent();
     }
+
+    public ICommand DeleteCommand
+    {
+        get => (ICommand)GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+    
+    public ICommand EditCommand
+    {
+        get => (ICommand)GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+    
+    public static readonly BindableProperty DeleteCommandProperty =
+        BindableProperty.Create(nameof(DeleteCommand), typeof(ICommand), typeof(ChampionItemList));
+    
+    public static readonly BindableProperty EditCommandProperty =
+        BindableProperty.Create(nameof(EditCommand), typeof(ICommand), typeof(ChampionItemList));
 }
